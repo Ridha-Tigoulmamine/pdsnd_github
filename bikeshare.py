6342377346cac70a6bf0576ad9e33a652e2c79d1
Ridha Tigoulamine <ridha.tigoulmamine@gmail.com>
2020-01-24 11:39:02 +0100

    “update_commit”
    add_version

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,8 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+print('Hello! welcome to US bikeshare project ')
+print('version 1.0')
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -15,7 +16,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
       city = input("\nWhich city would you like to filter by? New York City, Chicago or Washington?\n")
