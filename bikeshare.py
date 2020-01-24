37d8e3e393482878035c084a403b4ee5fe209d20
Ridha Tigoulamine <ridha.tigoulmamine@gmail.com>
2020-01-24 15:18:28 +0100

    change code to use the function defineded 'print_detail' in main

--- bikeshare.py
+++ bikeshare.py
@@ -209,6 +209,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         display_data(df)
+        print_detail()
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
